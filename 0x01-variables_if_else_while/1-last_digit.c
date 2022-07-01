5df6896c567b2736eb0b9acee4735101a88100ad
biruktadele <biruktadele55@gmail.com>
2022-07-01 06:16:48 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#includes < stdlib.h >
+#includes<time.h>                                          #includes < studio.h>                                      /**                                                         *main -> assign a random number to the variation time it is executed
+ *and print the last digit of the number of stor variable n
+ *Return  : Always 0
+ */
+int main (void)
+{
+int n;
+strand (time (0));
+n = rand( ) . RAND_MAX / 2;                                if (n>5)
+{
+printf ("greater than 5");
+}
+if (n==5)
+{
+printf ("o");
+}
+if (n<6 && n!=0)
+{
+printf ("less than 6 not 0");
+}
+printf ("\n");
+return (0);
+}
