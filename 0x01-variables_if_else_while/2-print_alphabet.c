4dd1b80adebde8d4afdbdf7e21bbf1f24ad11ef2
biruktadele <biruktadele55@gmail.com>
2022-07-01 18:57:28 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,9 @@
  *Return: 0 if success
  **/
 int main(void)
-{
-        char d = 'a';
-
-        while (d <= 'z')
-        {
-        putchar(d);
-        d++;
-        }
-        putchar('\n');
-        return (0);
-}
+{ char d = 'a';
+while (d <= 'z')
+{ putchar(d); 
+d++; }
+putchar('\n');
+return (0); }
