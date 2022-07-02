caf97d64aa2d2e72e0bd5d794171cd91344a8ec1
biruktadele <biruktadele55@gmail.com>
2022-07-01 17:26:29 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,30 @@
-#include <stdlib.h>                                                                  
-#include <time.h>                                                                    
-#include <stdio.h>                                                                   
-                                                                                     
-/**                                                                                  
- * main -> assign a randon number to the variable n each time it is executed and prin
-t                                                                                    
- * based a condition                                                                 
- * Return : always 0                                                                 
- */                                                                                  
-int main(void)                                                                       
-{                                                                                    
-        int n;                                                                       
-                                                                                     
-strand(time(0));                                                                     
-n = rand() - RAND_MAX / 2;                                                           
-if (n>0)                                                                             
-{                                                                                    
-        printf("%d is zero\n", n);                                                   
-}                                                                                    
-if (n==0)                                                                            
-{                                                                                    
-        printf("%d is zero\n", n);                                                   
-}                                                                                    
-if (n<0)                                                                             
-{                                                                                    
-        printf("%d is negative\n", n);                                               
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assign a random number to the variable n each time it is executed
+ * and prints out based on condition
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	        int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+   if (n > 0)
+{
+	printf("%d is %s\n", n, "positive");
 }
- return (0);                                         
+ else if (n < 0)
+{
+	 printf("%d is %s\n", n, "negative");
+}
+else
+{
+	printf("%d is %s\n", n, "zero");
+}
+return (0);
 }
