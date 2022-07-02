ab060d7609c5281fcf4d53dcab9028de5f97d696
biruktadele <biruktadele55@gmail.com>
2022-07-01 18:44:23 -0700

    git

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,9 @@
  *Return: 0 if success
  **/
 int main(void)
-{
-        char d = '0';
-        while (d <= '9')
-{
-putchar(d);
-d++;
-}
+{ char d = '0';
+while (d <= '9')
+{ putchar(d);
+d++; }
 putchar('\n');
-return (0);}
+return (0) ; }
