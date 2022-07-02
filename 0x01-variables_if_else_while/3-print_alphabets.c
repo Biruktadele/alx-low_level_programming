5fb1e8d98aee1ee0a1f4b3fbb3cad06cce219855
biruktadele <biruktadele55@gmail.com>
2022-07-01 18:51:49 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,10 @@
  *Return: 0
  **/
 int main(void)
-{
-        int ch;
-        for (ch = 97; ch <= 122; ch++)
-        {
-        putchar(ch);
-        }
-        for (ch = 65; ch <= 90; ch++)
-        {
-        putchar(ch);
-        }
-        putchar(10); /* this is the ASCII code for new line*/
-        return (0);
-}
+{ int ch;
+for (ch = 97; ch <= 122; ch++)
+{ putchar(ch); }
+for (ch = 65; ch <= 90; ch++)
+{ putchar(ch); }
+putchar(10); /* this is the ASCII code for new line*/
+return (0); }
