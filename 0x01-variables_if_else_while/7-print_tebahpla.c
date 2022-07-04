fa3691a37bf368f90368843835b2884357278da6
biruktadele <biruktadele55@gmail.com>
2022-07-04 05:58:21 -0700

    low lavel programing

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,15 @@
-include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
- *main - prints the lowercase alphabet in reverse, followed by a new line
+ * main - Prints the lowercase alphabet in reverse.
  *
- *Return: 0 if success
+ * Return: Always 0.
  */
 int main(void)
-{ char ch; 
-for (ch = 'z'; ch >= 'a'; ch--)
-{ putchar(ch); }
-putchar(10); }
+{
+	char letter;
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
