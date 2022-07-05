f2f82c59fb32249422e7230f07663b4ed922acdf
biruktadele <biruktadele55@gmail.com>
2022-07-05 15:01:01 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,14 @@
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
- * Return: 0
+* Return: 0
  */
 void print_alphabet_x10(void)
 {
-	char c, i;
-for (i = 0; i <= 9; i++)
-}
+int i;
+int c;
+for (i = 0; i < 10; i++)
+{
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
