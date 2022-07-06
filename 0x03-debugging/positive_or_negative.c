abdf954cf045f554f28e3e74f14099343b31c813
biruktadele <biruktadele55@gmail.com>
2022-07-06 06:58:53 -0700

    alx-low_level_programming

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdio.h>
-
-void positive_or_negative(int i) {
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; zero, positive or negative
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
+void positive_or_negative(int i)
+{
 int n;
 srand(time(0))
 n = rand() - RAND_MAX / 2;
@@ -15,18 +29,5 @@ if (n < 0)
 printf("%d is negative\n", i);
 }
 return (0);
-}
-/**
- * main - Entry point
- *
- * Description: print value of n satus; zero, positive or negative
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-( int i;
-  i = 0;
-positive_or_negative(i);
-return (0):
 }
 
