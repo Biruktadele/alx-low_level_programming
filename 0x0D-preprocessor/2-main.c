3461f5866ce871a0c377c1a9dd0e56b82c442438
biruktadele <biruktadele55@gmail.com>
2022-08-01 13:13:55 -0700

    ALX-LOW_LEVEL_PRORGRAMING

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints the name of the file it was compiled
+ * from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"2-main.c" [New File]                                             0,0-1         All
+
